506a5e1f6611ade413563a77406c9422c438a511
RonZhang724 <ronzhang@utexas.edu>
2021-10-04 22:54:02 -0500

    shorter solution for find least

--- 0_binary_search/least_position.py
+++ 0_binary_search/least_position.py
@@ -1,16 +1,24 @@
 def find_rotate_index(nums, left, right):
-    if left == right or nums[left] < nums[right]:
-        return 0
-
-    while left <= right:
+    while left < right:
         mid = (left + right) // 2
-        if nums[mid] > nums[mid + 1]:
-            return mid + 1
+        if nums[mid] < nums[right]:
+            right = mid
         else:
-            if nums[mid] < nums[left]:
-                right = mid - 1
-            else:
-                left = mid + 1
+            left = mid + 1
+    return left
+
+
+def find_rotate_index_with_duplicates(nums, left, right):
+    while left < right:
+        mid = (left + right) // 2
+        if nums[left] == nums[mid] == nums[right]:
+            left += 1
+            right -= 1
+        elif nums[mid] <= nums[right]:
+            right = mid
+        else:
+            left = mid + 1
+    return left
 
 
 arr = [1, 2, 3, 0]
@@ -18,3 +26,6 @@ print(find_rotate_index(arr, 0, len(arr) - 1))
 
 arr = [1]
 print(find_rotate_index(arr, 0, len(arr) - 1))
+
+arr = [3, 1, 3, 3, 3]
+print(find_rotate_index_with_duplicates(arr, 0, len(arr) - 1))
